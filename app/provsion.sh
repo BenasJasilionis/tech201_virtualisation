85ba1f21dd46e8644c1128c12ec4eb6310577218
BenasJasilionis <benas.jasilionis@gmail.com>
2023-02-21 14:18:24 +0000

    changed provsion.sh

--- app/provsion.sh
+++ app/provsion.sh
@@ -1,21 +1,44 @@
 #!/bin/bash
 
+# Update and upgrade
 sudo apt-get update -y
 sudo apt-get upgrade -y
+
+
+# Install Nginx
 sudo apt-get install nginx -y
-sudo systemctl start nginx
-sudo systemctl enable
-sudo apt-get install python -y
+
+# Enable or Start Nginx
+sudo systemctl enable nginx -y
+
+
+# Reverse proxy setup:
 # Remove default nginx configuration
 sudo rm /etc/nginx/sites-available/default
 # Copy the reverse proxy file containing the relevant code into that location
-sudo cp app/reverse_proxy /etc/nginx/sites-available/default
+sudo cp /home/ubuntu/app/reverse_proxy /etc/nginx/sites-available/default
 # Restart nginx to confirm changes
 sudo systemctl restart nginx -y
-# install nodejs
+
+# Install nodejs dependencies
 sudo apt-get install python-software-properties
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+
+# Overwrite with desired version of nodejs
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+
+# Install nodejs
 sudo apt-get install nodejs -y
 
-# install pm2
+# Install pm2
 sudo npm install pm2 -g
+
+# Install app
+npm install
+
+# Environmental vabriable
+
+
+# Enable reverse proxy
+sudo systemctl restart nginx
+
+# Seed the database
